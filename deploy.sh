c42d72c7a36ce8c1160a8c6e03314ca7f5798319
Cedric Fung <cedric@vec.io>
2018-06-01 10:32:54 +0800

    the logs is deployed in Google Cloud Storage

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+set -e
+
+rm -r dist/
+
 hugo
 minify -r --type json -o dist/ public
-scp dist/index.json one@mixin-logs:html/
+
+gzip dist/index.json
+mv dist/index.json.gz dist/logs.json
+
+gsutil -h "Cache-Control:no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0" -h "Content-Encoding:gzip" -m cp dist/logs.json gs://mixin-one/assets/
